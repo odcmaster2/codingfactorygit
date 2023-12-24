fe3fe5622494ce969fab17fdc9de76d94321e843
odcmaster2 <odcmaster2@gmail.com>
2023-12-24 19:36:21 +0200

    sumApp.java

--- Chapter1/SumApp.java
+++ Chapter1/SumApp.java
@@ -1,3 +1,4 @@
+
 package gr.aueb.cf.ch1;
 
 public class SumApp {
@@ -6,7 +7,7 @@ public class SumApp {
         int num2 = 30;
         int summarize1plus2 =0;
         summarize1plus2=num1 + num2;
-        System.out.printf(" Το αποτέλεσμα της πρόσθεσης είναι ίσο με :"+ summarize1plus2)
+        System.out.printf(" Το σωστο  αποτέλεσμα της πρόσθεσης είναι ίσο με :"+ summarize1plus2)
         ;
     }
-}
+} 
